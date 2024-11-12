c06a2c276b0f74d13b2add781cace3a73b0f9fbe
BH <hiantonio1107@gmail.com>
2024-11-12 15:50:22 +0900

    chore: 함수화 및 환경변수 설정

--- database.py
+++ database.py
@@ -4,25 +4,34 @@ import pandas as pd
 from langchain_community.document_loaders.csv_loader import CSVLoader
 from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
+import os
 
 load_dotenv()
 
-data_csv=pd.read_csv(r'C:\Users\USER\Desktop\MyCode_BH\SkAiCampMyData\3rdproject\laptops_data.csv', encoding='utf-8')
+csv_read_path = os.getenv("csv_read_path")
+faiss_save_path = os.getenv("faiss_save_path")
 
-loader = CSVLoader(r'C:\Users\USER\Desktop\MyCode_BH\SkAiCampMyData\3rdproject\laptops_data.csv', encoding='utf-8')
-documents = loader.load()
 
-for i, document in enumerate(documents):
-    document.page_content = document.page_content.replace('\ufeffname:', '').replace('\n','').replace('content:','')
-    document.metadata.update({'source': data_csv.name.iloc[i]})
-    document.metadata.update({'price': data_csv.price.iloc[i]})
+def laptop_data_to_faiss(csv_read_path, faiss_save_path):
 
-db = FAISS.from_documents(
-    documents=documents,
-    embedding=OpenAIEmbeddings(model='text-embedding-3-small')
-)
+    data_csv=pd.read_csv(csv_read_path, encoding='utf-8')
 
-db.save_local(
-    folder_path='./data/db',
-    index_name='faiss_index'
-)
+    loader = CSVLoader(csv_read_path, encoding='utf-8')
+    documents = loader.load()
+
+    for i, document in enumerate(documents):
+        document.page_content = document.page_content.replace('\ufeffname:', '').replace('\n','').replace('content:','')
+        document.metadata.update({'source': data_csv.name.iloc[i]})
+        document.metadata.update({'price': data_csv.price.iloc[i]})
+
+    db = FAISS.from_documents(
+        documents=documents,
+        embedding=OpenAIEmbeddings(model='text-embedding-3-small')
+    )
+
+    db.save_local(
+        folder_path=faiss_save_path,
+        index_name='faiss_index'
+    )
+
+laptop_data_to_faiss(csv_read_path,faiss_save_path)
