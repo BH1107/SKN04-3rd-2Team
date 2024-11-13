faf4a5ce0158bd32b317b6c585a22085afdd1bd7
dldkddkd <jisoolee1997@naver.com>
2024-11-13 11:33:28 +0900

    chore: 임시로 query 값 넣어서 실행하는 코드 삭제

--- retrieval.py
+++ retrieval.py
@@ -5,9 +5,6 @@ from langchain.chains import RetrievalQA
 
 
 def retrieve_and_answer(fetch_k=20, k=1, lambda_mult=0.3, query=''):
-    if query == '':
-        query='UX5406SA-ABCDFSD 한글로 사양알려줘'
-
     db = FAISS.load_local(
         folder_path='./data/db',
         index_name='faiss_index',
